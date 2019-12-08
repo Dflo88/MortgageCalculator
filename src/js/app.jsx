385c879575e26f9ec120bcb8905f9be6ae8a21e3
Diego <dflores7237@gmail.com>
2019-12-08 14:27:21 -0800

    cleaned up handleChange function and added comments.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,42 +4,38 @@ export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      balValue: "",
-      rateValue: "",
-      termValue: 15,
+      balance: "",
+      rate: "",
+      term: 15,
       monthlyPayment: ""
     }
-    this.handleBalChange = this.handleBalChange.bind(this);
-    this.handleRateChange = this.handleRateChange.bind(this);
-    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.calculate = this.calculate.bind(this);
-    
   }
-  handleBalChange(event){
-    this.setState({balValue: event.target.value});
+  handleChange(event){
+    this.setState({[event.target.name]: event.target.value});
   }
-  handleRateChange(event){
-    this.setState({rateValue: event.target.value});
-  }
-  handleTermChange(event){
-    this.setState({termValue: event.target.value});
-  }
-  
   calculate(){
-    const monthlyRate = (this.state.rateValue/12)/100;
-    const monthlyTerms = this.state.termValue*12;
-    var mortgage = (this.state.balValue)*(((Math.pow((1+monthlyRate),monthlyTerms))*monthlyRate)/((Math.pow((1+monthlyRate),monthlyTerms))-1));
+    // monthlyRate will convert an annual percentage rate to a monthly percentage rate as a decimal.
+    const monthlyRate = (this.state.rate/12)/100;
+    // monthlyTerms converts the term in years to the number of monthly payments over the loan term.
+    const monthlyTerms = this.state.term*12;
+    // the variable mortgage uses the monthlyRate and monthlyTerms along with the principal balance to determine the monthly payment
+    // the formula used in mortage is: M = P[r(1+r)^n/((1+r)^n)-1)]
+    // where M is the total monthly mortgage payment
+    // P is the principal loan amount
+    // r is the monthly interest rate
+    // n is the number of payments over the loan's lifetime
+    var mortgage = (this.state.balance)*(((Math.pow((1+monthlyRate),monthlyTerms))*monthlyRate)/((Math.pow((1+monthlyRate),monthlyTerms))-1));
     this.setState({monthlyPayment: `$ ${mortgage.toFixed(2)} is your payment.`});
   }
   render() {
-    
-
     return (
       <div className='container'>
-        <h1>Mortgage Calculator</h1>
-        {<input name="balance" type="number" placeholder="Current Balance" value={this.state.balValue} onChange={this.handleBalChange}/>}
-        {<input name="rate" type="number" step="0.01" placeholder="Current Rate" value={this.state.rateValue} onChange={this.handleRateChange}/>}
-        {<select name="term" placeholder="Terms in Years" value={this.state.termValue} onChange={this.handleTermChange}>
+        <h3>Mortgage Calculator</h3>
+        {<input name="balance" type="number" placeholder="Current Balance" value={this.state.balance} onChange={this.handleChange}/>}
+        {<input name="rate" type="number" step="0.01" placeholder="Current Rate" value={this.state.rate} onChange={this.handleChange}/>}
+        {<select name="term" placeholder="Terms in Years" value={this.state.term} onChange={this.handleChange}>
           <option>15</option>
           <option>30</option>
           </select>}
